cc96d3676998cd445b0d5313f1ea6ae65e6201fd
Konstantin Shapoval <konstantin.shapoval@leasoft.org>
2020-10-06 15:36:07 +0300

    Change version. Add expanded error description.

--- includes/modules/payment/emspay_bancontact.php
+++ includes/modules/payment/emspay_bancontact.php
@@ -121,7 +121,11 @@ class emspay_bancontact {
     if ( !is_array( $emspay_order ) or array_key_exists( 'error', $emspay_order) or $emspay_order['status'] == 'error' ) {
       // TODO: Remove this? I don't know if I like it removing orders, or make it optional
       $this->tep_remove_order( $insert_id, $restock = true );
-      tep_redirect( tep_href_link( FILENAME_CHECKOUT_PAYMENT, 'error_message=' . urlencode( "Error placing emspay order" ), 'SSL' ) );
+        $error_message = "Error placing emspay order";
+        if ($emspay_order['transactions'][0]['reason']) {
+            $error_message .= ": ".$emspay_order['transactions'][0]['reason'];
+        }
+        tep_redirect( tep_href_link( FILENAME_CHECKOUT_PAYMENT, 'error_message=' . urlencode($error_message), 'SSL' ));
     }
     else {
       tep_redirect( $emspay_order['transactions'][0]['payment_url'] );
